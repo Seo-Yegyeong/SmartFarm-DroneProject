10c189ce4a91ac5e59192e7fff2a858f06ee124a
YegyeongSeo <seoye6618@gmail.com>
2022-11-19 21:25:25 +0900

    new repository for Go-Back-N ARQ Programming

---
+++ GBN_ARQ_UDP/hw1_server.c
@@ -0,0 +1,92 @@
+//--------------------------
+//Socket Programming
+//udp_server.c
+//--------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define BUFSIZE 550
+#define FILENAME 30
+void error_handling(char * message);
+
+int main(int argc, char ** argv){
+	int serv_sock;
+
+	int str_len, num = 0;
+	int i;
+	char * parseMsg;
+	FILE *file;
+	
+	char filename[FILENAME];
+	char message[BUFSIZE];
+	
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in clnt_addr;
+	int clnt_addr_size;
+	
+
+	if(argc!=2) {
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	serv_sock = socket(PF_INET, SOCK_DGRAM, 0);
+	if(serv_sock == -1)
+		error_handling("UDP socket() error");
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(atoi(argv[1]));
+
+	if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
+		error_handling("bind() error");
+
+	//first time
+	clnt_addr_size = sizeof(clnt_addr);
+	
+	str_len = recvfrom (serv_sock, filename, FILENAME, 0, (struct sockaddr*) &clnt_addr, &clnt_addr_size);
+	filename[str_len] = '\0';
+	printf("%s\n==========", filename);
+	if((file = fopen(filename, "w+")) == NULL){
+		printf("file open error!\n");
+	}
+
+
+	while(1){
+		clnt_addr_size = sizeof(clnt_addr);
+        str_len = recvfrom (serv_sock, message, BUFSIZE, 0, (struct sockaddr*) &clnt_addr, &clnt_addr_size);
+        message[str_len] = '\0';
+ 		if(strcmp(filename, message)==0)
+			continue;
+
+		fprintf(file, "%s\n", message);
+		//for test
+		printf("%s\n==========================\n", message);
+		//for test
+   
+   		sendto(serv_sock, message, str_len, 0, (struct sockaddr*) &clnt_addr, sizeof(clnt_addr));
+      
+    }
+/*
+	parseMsg = strtok(fmessage, "|");
+	i=0;
+	while(parseMsg != NULL){
+		i++;
+		printf("Received message %d: %s\n", i, parseMsg);
+		parseMsg = strtok(NULL, "|");
+	}
+*/
+
+	return 0;
+}
+
+void error_handling(char *message){
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
